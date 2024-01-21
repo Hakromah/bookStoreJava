bf39b471ec1b3d151279705c34020e355e5dcfe6
Hassan <kkromah12@gmail.com>
2024-01-22 02:03:54 +0300

    Database connected and data saved in database and fetched

--- src/main/java/com/bookStore/controller/BookController.java
+++ src/main/java/com/bookStore/controller/BookController.java
@@ -1,13 +1,89 @@
 package com.bookStore.controller;
 
+import org.springframework.ui.Model;
+//import ch.qos.logback.core.model.Model;
+import com.bookStore.entity.Book;
+import com.bookStore.entity.MyBookList;
+import com.bookStore.service.BookService;
+import com.bookStore.service.MyBookListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class BookController {
 
-    @GetMapping("/home")
+    //creat BookService Object & pass it to addBooks method that will immplements save action
+    @Autowired
+    private BookService service;
+
+    @Autowired
+    private MyBookListService myBookListService;
+
+    @GetMapping("/")
     public String home() {
         return "home";
     }
+
+    @GetMapping("/book_register")
+    public String bookRegister() {
+        return "bookRegister";
+    }
+
+
+    // fetch data from databases and send it to Available book list
+    @GetMapping("/available_books")
+    public ModelAndView getAllBook() {
+        List<Book> list = service.getAllBook();
+        //ModelAndView m = new ModelAndView();
+        //m.setViewName("availableBooks");
+        //m.addObject("book",list);
+        //return m;
+        return new ModelAndView("availableBooks", "book", list);
+    }
+
+    @PostMapping("/save")
+    public String addBook(@ModelAttribute Book b) {
+        service.save(b);
+        return "redirect:/available_books";
+    }
+
+    @GetMapping("/my_books")
+    public String getMyBooks(Model model) {
+        List<MyBookList> list = myBookListService.getAllMyBooks();
+        model.addAttribute("book",list);
+        return "myBooks";
+    }
+
+    //Let save selected book from available book to MyBookList
+    @RequestMapping("/mylist/{id}")
+    public String getMyList(@PathVariable("id") int id) {
+        Book b = service.getBookById(id);
+        MyBookList mb = new MyBookList(b.getId(), b.getName(), b.getAuthor(), b.getPrice());
+        myBookListService.saveMyBooks(mb);
+        return "redirect:/my_books";
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
